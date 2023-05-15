d83bf087ea2aa0576d03e6bc3d38a2452e38b4a2
KarlaB3 <k.bucoy@gmail.com>
2023-05-15 16:21:32 +1000

    created new blog form with onChange event

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("");
+
     return (
         <div className="create">
             <h2>Create new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input type="text" required value={title} onChange={(event) => setTitle(event.target.value)}/>
+                <label>Blog body:</label>
+                <textarea required value={body} onChange={(event) => setBody(event.target.value)}></textarea>
+                <label>Blog author:</label>
+                <select value={author} onChange={(event) => setAuthor(event.target.value)}>
+                    <option value="Noob KB">Noob KB</option>
+                    <option value="Curious KB">Curious KB</option>
+                    <option value="Reflective KB">Reflective KB</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     );
 }
