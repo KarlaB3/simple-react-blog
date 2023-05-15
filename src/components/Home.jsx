b69c54736c3673ceefc254eff6baca9976cfe61a
KarlaB3 <k.bucoy@gmail.com>
2023-05-15 14:29:09 +1000

    reconfigured fetch into a custom hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,8 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch("http://localhost:8000/blogs")
-                .then(response => {
-                    if (!response.ok) {
-                        throw Error("Could not fetch data");
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsLoading(false);
-                    setError(null);
-                })
-                .catch(error => {
-                    setIsLoading(false);
-                    setError(error.message);
-                })
-        }, 1000);
-    }, []);
+    const {data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs");
 
     return (
         <div className="home">
