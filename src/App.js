c8638d9001990af5c169656a0271025627e0fec7
KarlaB3 <k.bucoy@gmail.com>
2023-05-15 15:05:29 +1000

    commenced routes using React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import CreateBlog from './components/CreateBlog'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateBlog />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/CreateBlog.jsx
@@ -0,0 +1,9 @@
+const CreateBlog = () => {
+    return (
+        <div className="create">
+            <h2>Create new blog</h2>
+        </div>
+    );
+}
+
+export default CreateBlog;
