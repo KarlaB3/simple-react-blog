37616b9aa56ae3836f0c5b6c6d64d9e655cb73ee
KarlaB3 <k.bucoy@gmail.com>
2023-05-15 14:16:58 +1000

    implemented json server and error handling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error("Could not fetch data");
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(error => {
+                    setIsLoading(false);
+                    setError(error.message);
+                })
+        }, 1000);
+    }, []);
+
     return (
         <div className="home">
-            <h2>Home Page</h2>
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 }
